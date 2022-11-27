b8c4457adf6e3f0c83db5473cb88ada6e7bda765
wangzhuolan <1437286101@qq.com>
2022-11-27 18:13:08 +0800

    11

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -1,238 +1,146 @@
-#include "cachelab.h"
+#include <fcntl.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 
-#define STANDARD_HELP "Usage: %s [-hv] -s <num> -E <num> -b <num> -t <file>\nOptions:\n-h         Print this help message.\n-v         Optional verbose flag.\n-s <num>   Number of set index bits.\n-E <num>   Number of lines per set.\n-b <num>   Number of block offset bits.\n-t <file>  Trace file.\nExamples:\n    linux>  %s -s 4 -E 1 -b 4 -t traces/yi.trace\n  linux>  %s -v -s 8 -E 2 -b 4 -t traces/yi.trace\n"
-#define BUFFER_SIZE 1024
-#define OP_LENGTH    128
+#include "cachelab.h"
 
-int hit_count = 0, miss_count = 0, eviction_count = 0;
-int timestamp = 0;
+// 因为那些帮助信息实在是繁琐，所以我不想写emm,当然这不影响这份代码通过测试
+void usage(void) { exit(1); }
+int verbose = 0, s, E, b, S, B;  // opt的变量， 定义了cache的大小
 
-typedef struct per_block
+int T = 0;  //这是全局的一个时刻表，第一个访存指令的时刻为1，之后每次数据访存都累加1
+typedef __uint64_t uint64_t;
+// 定义行
+typedef struct lineNode {  // 不需要block， 用不上这个lab
+    int t;                 // 时刻，即是valid 也是 LRU的判断标志
+    uint64_t tag;          // 标记
+} * groupNode;             // 组的定义很明显，就是行的数组，也就是struct lineNode*
+
+enum Category { HIT, MISS, EVICTION };
+unsigned int result[3];  // 结果，写成数组是为了 简洁
+const char *categoryString[3] = {"hit ", "miss ", "eviction "};
+groupNode *cache;  // 组的数组 也就是 cache
+void init();       // 初始化cache
+// 释放内存，其实这个函数写不写都无所谓，进程一死空间自然没，不过手动释放是一个好的习惯:)
+void destory();
+FILE *opt(int argc, char **argv);                           // 处理那些命令行选项
+void findCache(uint64_t tag, int group_pos, char *result);  // 核心函数，很短，按照定义处理即可
+
+int main(int argc, char **argv)
 {
-    bool isVaild;
-    int tag;
-    int timestamp;
-} per_block;
+    FILE *tracefile = opt(argc, argv);
+    // 现在我们得到了S，E，B,现在可以构造cache了
+    init();
+    // 接下来处理每一条指令
+    char oper[2];
+    uint64_t address;
+    int size;  //访问的地址和字节数
+               // 用%s 可以忽略空格，不需要考虑那些空格了
+    while (fscanf(tracefile, "%s %lx,%d\n", oper, &address, &size) == 3) {
+        if (oper[0] == 'I') continue;                  // 忽略I
+        int group_pos = (address >> b) & ~(~0u << s);  // 从 第 b位开始取，取s位
+        uint64_t address_tag = address >> (b + s);     // b + s之后的位都是
+        char resultV[20];                              // 为了 -v 设置的string显示
+        memset(resultV, 0, sizeof resultV);
+        ++T;
+        findCache(address_tag, group_pos, resultV);
+        if (oper[0] == 'M') findCache(address_tag, group_pos, resultV);  // M需要两次
 
-typedef per_block* set;
-
-typedef struct cache
-{
-    int nlines;
-    int nsets;
-    int bblocks;
-    int bsets;
-    set* head;
-} cache;
-
-typedef struct operation
-{
-    char op;
-    int size;
-    unsigned long address;
-} operation;
-
-static inline unsigned gets_s(unsigned long address, int s, int b)
-{
-    return (address >> b) % (2 << (s - 1));
-}
-
-static inline unsigned gets_b(unsigned long address, int s, int b)
-{
-    return address % (2 << (b - 1));
-}
-
-static inline unsigned gets_t(unsigned long address, int s, int b)
-{
-    return address >> (s + b);
-}
-
-cache allocate_set(int s, int b, int e)
-{
-    int nsets = 1 << s;
-    set* head = calloc(nsets, sizeof(set));
-    for (int i = 0; i < nsets; ++i)
-        head[i] = calloc(e, sizeof(per_block));
-
-    cache c = { e, nsets, b, s, head };
-    return c;
-}
-
-void free_set(cache* c)
-{
-    for (int i = 0; i < c->nsets; ++i)
-    {
-        free(c->head[i]);
-        c->head[i] = NULL;
+        if (verbose) fprintf(stdout, "%s %lx,%d %s\n", oper, address, size, resultV);
     }
-    free(c->head);
-    c->head = NULL;
-}
+    printSummary(result[0], result[1], result[2]);
+    // destory();
 
-operation fetch_operation(FILE* pfile)
-{
-    char temp[OP_LENGTH];
-    operation oper = { 0, 0, 0 };
-    if(!fgets(temp, OP_LENGTH, pfile))
-        return oper;
-    sscanf(temp, "\n%c %lx,%d", &oper.op, &oper.address, &oper.size);
-    return oper;
-}
-
-per_block* find_match_block(per_block* pb, int e, int tag)
-{
-    for (int i = 0; i < e; ++i)
-        if (pb[i].tag == tag && pb[i].isVaild)
-            return pb + i;
-    return NULL;
-}
-
-per_block* find_evict_block(per_block* pb, int e)
-{
-    int index = 0;
-    for (int i = 1; i < e; ++i)
-        if (pb[i].timestamp < pb[index].timestamp)
-            index = i;
-    return pb + index;
-}
-
-per_block* find_new_block(per_block* pb, int e)
-{
-    for (int i = 0; i < e; ++i)
-        if (!(pb[i].isVaild))
-            return pb + i;
-
-    return NULL;
-}
-
-void load_or_store(cache* c, operation* op, bool isDisplay, bool isMainEnvironment)
-{
-    int s = gets_s(op->address, c->bsets, c->bblocks);
-    int t = gets_t(op->address, c->bsets, c->bblocks);
-
-    if (isDisplay && isMainEnvironment)
-        printf("%c %lx,%d ", op->op, op->address, op->size);
-
-    per_block* appropriate_tag = find_match_block(c->head[s], c->nlines, t);
-    if (appropriate_tag)
-    {
-        ++hit_count;
-        if (isDisplay)
-            printf("hit ");
-    }
-    else
-    {
-        ++miss_count;
-        if (isDisplay)
-            printf("miss ");
-
-        appropriate_tag = find_new_block(c->head[s], c->nlines);
-        if (appropriate_tag)
-            appropriate_tag->isVaild = true;
-        else
-        {
-            ++eviction_count;
-            appropriate_tag = find_evict_block(c->head[s], c->nlines);
-            if (isDisplay)
-                printf("eviction ");
-        }
-
-        appropriate_tag->tag = t;
-    }
-
-    appropriate_tag->timestamp = timestamp;
-    timestamp++;
-
-    if (isDisplay && isMainEnvironment)
-        printf("\n");
-}
-
-void modify(cache* sh, operation* op, bool isDisplay)
-{
-    if (isDisplay)
-        printf("%c %lx,%d ", op->op, op->address, op->size);
-    op->op = 'L';
-    load_or_store(sh, op, isDisplay, false);
-    op->op = 'S';
-    load_or_store(sh, op, isDisplay, false);
-    if (isDisplay)
-        printf("\n");
-}
-
-void get_arguments(int argc, char** argvs, int* s, int* e, int* b, char** path, bool* isDisplay)
-{
-    for (int i = 1; i < argc;)
-    {
-        if (strcmp(argvs[i], "-s") == 0)
-        {
-            *s = atoi(argvs[i + 1]);
-            i += 2;
-        }
-        else if (strcmp(argvs[i], "-E") == 0)
-        {
-            *e = atoi(argvs[i + 1]);
-            i += 2;
-        }
-        else if (strcmp(argvs[i], "-b") == 0)
-        {
-            *b = atoi(argvs[i + 1]);
-            i += 2;
-        }
-        else if (strcmp(argvs[i], "-t") == 0)
-        {
-            *path = argvs[i + 1];
-            i += 2;
-        }
-        else if (strcmp(argvs[i], "-v") == 0)
-        {
-            *isDisplay = true;
-            ++i;
-        }
-        else
-        {
-            printf("%s: invalid option -- %s\n", argvs[0], argvs[i]);
-            printf(STANDARD_HELP, argvs[0], argvs[0], argvs[0]);
-            exit(1);
-        }
-    }
-}
-
-int main(int argc, char** argvs)
-{
-    for (int i = 0; i < argc; ++i)
-    {
-        if (strcmp(argvs[i], "-h") == 0)
-        {
-            printf(STANDARD_HELP, argvs[0], argvs[0], argvs[0]);
-            return 0;
-        }
-    }
-
-    char* path = NULL;
-    bool isDisplay = false;
-    int s = 0, e = 0, b = 0;
-    get_arguments(argc, argvs, &s, &e, &b, &path, &isDisplay);
-    cache sh = allocate_set(s, b, e);
-    FILE* pfile = fopen(path, "r");
-    setvbuf(pfile, NULL, _IOFBF, BUFFER_SIZE);
-
-    operation o;
-    while ((o = fetch_operation(pfile)).op)
-    {
-        if (o.op == 'M')
-            modify(&sh, &o, isDisplay);
-        else if (o.op == 'L' || o.op == 'S')
-            load_or_store(&sh, &o, isDisplay, true);
-        else if (o.op == 'I')
-            continue;
-    }
-    
-    fclose(pfile);
-    free_set(&sh);
-    printSummary(hit_count, miss_count, eviction_count);
     return 0;
+}
+
+// 初始化整个cache
+void init()
+{
+    cache = (groupNode *)malloc(sizeof(groupNode) * S);
+    for (int i = 0; i < S; ++i) {
+        cache[i] = (struct lineNode *)malloc(sizeof(struct lineNode) * E);  // 每一组有E行
+        for (int j = 0; j < E; ++j) cache[i][j].t = 0;  // 初始化时，全部都未访问
+    }
+}
+
+// category是缓存的种类，resultV是main传下来的，为了verbose的输出
+void setResult(groupNode group, enum Category category, int tag, int pos, char *resultV)
+{
+    ++result[category];
+    group[pos].tag = tag;
+    group[pos].t = T;
+    if (verbose) strcat(resultV, categoryString[category]);
+}
+
+// 这是这整一个代码的核心部分了 具体思路为：
+// 遍历这个组的所有行，然后看一下是否命中，最后再进行相应的操作即可
+void findCache(uint64_t tag, int group_pos, char *resultV)
+{
+    groupNode group = cache[group_pos];
+    int min_t_pos = 0, empty_line = -1;
+    for (int i = 0; i < E; ++i) {
+        struct lineNode line = group[i];
+        if (!line.t)
+            empty_line = i;  // 有空行
+        else {
+            if (line.tag == tag) {  // 命中，设置hit
+                setResult(group, HIT, tag, i, resultV);
+                return;
+            }
+            if (group[min_t_pos].t > line.t)
+                min_t_pos = i;  // 取最小的时刻值，也就是最近最少访问的了
+        }
+    }
+    setResult(group, MISS, tag, empty_line, resultV);
+    if (empty_line == -1)  //要读或者写但是没有一个 空行 说明得发生eviction
+        setResult(group, EVICTION, tag, min_t_pos, resultV);
+}
+
+void destory()
+{
+    // printf("%d\n", S);
+    for (int i = 0; i < S; ++i) {
+        free(cache[i]);  // 实在是很奇怪，我不知道这为什么会报错--？
+        // 如果你知道的话，希望你能告诉我^_^(linxi177229@gmail.com)
+    }
+    free(cache);
+}
+
+FILE *opt(int argc, char **argv)
+{
+    FILE *tracefile;
+    /* Parse the command line 这里c用int是为了保证兼容性，因为有的系统char是unsigned的*/
+    for (int c; (c = getopt(argc, argv, "hvsEbt")) != EOF;) {
+        switch (c) {
+            case 'h': /* print help message */
+                usage();
+                break;
+            case 'v': /* emit additional diagnostic info */
+                verbose = 1;
+                break;
+            case 't': /* 文件 */
+                tracefile = fopen(argv[optind], "r");
+                if (tracefile == NULL) usage();
+                break;
+            case 's':  // 组数的位
+                s = atoi(argv[optind]);
+                if (s <= 0) usage();
+                S = 1 << s;
+                break;
+            case 'E':  // 每一组的行数
+                E = atoi(argv[optind]);
+                if (E <= 0) usage();
+                break;
+            case 'b':
+                b = atoi(argv[optind]);
+                if (b <= 0) usage();
+                B = 1 << b;
+                break;
+        }
+    }
+    // printf("-%d 4 -%d 1 -%d 4 -t \n", s, E, b);
+    return tracefile;
 }
